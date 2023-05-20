059e00893bf939f72a86a7e18dc9c4b033995769
OmarAbaroa <alvaro1295@hotmail.com>
2023-05-20 12:26:18 -0600

    ISG e ISR

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -115,18 +115,31 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonIds.map( id => ({
       params: { id }
     })),
-    fallback: false
+    // fallback: false If the page has invalid parameter show 404
+    fallback: 'blocking'
   }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const { id } = params as { id : string };
+
+  const pokemon = await getPokemonInfo( id )
+
+  if( !pokemon ){
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
   
   return {
     props: {
-      pokemon: await getPokemonInfo( id )
-    }
+      pokemon
+    },
+    revalidate: 86400 // Seconds in a day (ISR)
   }
 }
 
